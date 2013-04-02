24d0aef07ccf9cc14ecb90267bbeeacf9c05307b
Greg Wilson <gvwilson@third-bit.com>
2013-04-02 15:33:06 -0400

    Radically simplified the clustering example, and finished writing the instructors' guide for sets and dictionaries

--- setdict/phylogen.py
+++ setdict/phylogen.py
@@ -1,70 +1,44 @@
-def combos(species):
-    '''Generate all combinations of species in normalized order.'''
-
+def main(species, scores):
     result = []
-    for i in range(len(species)):
-        for j in range(i+1, len(species)):
-            result.append((species[i], species[j]))
+    while len(species) > 1:
+        left, right = find_min_pair(species, scores)
+        result.append(make_pair(left, right))
+        species -= {left, right}
+        make_new_pairs(species, scores, left, right)
+        species.add(make_name(left, right))
     return result
 
 def find_min_pair(species, scores):
-    '''Find a minimum-value pair of species in the scores table.'''
-
-    min_pair, min_val = None, None
-    for pair in combos(species):
-        assert pair in scores, 'Pair (%s, %s) not in scores' % pair
-        if (min_val is None) or (scores[pair] < min_val):
-            min_pair, min_val = pair, scores[pair]
-    assert min_val is not None, 'No minimum value found in scores'
+    min_pair = None
+    min_val = None
+    for left in species:
+        for right in species:
+            if left < right:
+                this_pair = make_pair(left, right)
+                if (min_val is None) or (scores[this_pair] < min_val):
+                    min_pair = this_pair
+                    min_val = scores[this_pair]
     return min_pair
 
-def create_new_parent(scores, pair):
-    '''Create record for new parent.'''
-
-    parent = '[%s %s]' % pair
-    height = scores[pair] / 2.
-    return parent, height
-
-def remove_entries(species, scores, pair):
-    '''Get combined species out of species list and scores.'''
-
-    left, right = pair
-    species.remove(left)
-    species.remove(right)
-    old_score = scores[pair]
-    del scores[pair]
-    return old_score
+def make_new_pairs(species, scores, left, right):
+    for current in species:
+        left_score = scores[make_pair(current, left)]
+        right_score = scores[make_pair(current, right)]
+        new_score = (left_score + right_score) / 2.0
+        scores[make_pair(current, make_name(left, right))] = new_score
 
 def make_pair(left, right):
-    '''Make an ordered pair.'''
+    if left < right:
+        return (left, right)
+    else:
+        return (right, left)
 
-    if left < right:  return (left, right)
-    else:             return (right, left)
-
-# Pull entry combining half of new pair with something else.
-def tidy_up(scores, old, other):
-    pair = make_pair(old, other)
-    score = scores[pair]
-    del scores[pair]
-    return score
-
-def update(species, scores, pair, parent, old_parent_score):
-    '''Remove two species from the scores table.'''
-
-    left, right = pair
-    for other in species:
-        old_left_score = tidy_up(scores, left, other)
-        old_right_score = tidy_up(scores, right, other)
-        new_pair = make_pair(parent, other)
-        new_score = (old_left_score + old_right_score - old_parent_score) / 2.
-        scores[new_pair] = new_score
-    
-    species.append(parent)
-    species.sort()
+def make_name(left, right):
+    return '<%s, %s>' % make_pair(left, right)
 
 if __name__ == '__main__':
 
-    species = ['human', 'mermaid', 'vampire', 'werewolf']
+    species = {'human', 'mermaid', 'werewolf', 'vampire'}
 
     scores = {
         ('human',   'mermaid')  : 12,
@@ -75,9 +49,5 @@ if __name__ == '__main__':
         ('vampire', 'werewolf') :  6
     }
 
-    while len(scores) > 0:
-        min_pair = find_min_pair(species, scores)
-        parent, height = create_new_parent(scores, min_pair)
-        print parent, height
-        old_score = remove_entries(species, scores, min_pair)
-        update(species, scores, min_pair, parent, old_score)
+    order = main(species, scores)
+    print order
