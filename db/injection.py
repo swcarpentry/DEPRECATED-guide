2a5c0f6aee2728ba6de45b371ec037e0197cab40
Greg Wilson <gvwilson@third-bit.com>
2013-04-16 11:58:26 -0400

    Trying to finish off discussion of databases

--- db/injection.py
+++ db/injection.py
@@ -1,13 +1,13 @@
 import sys
 import sqlite3
 
-query = "select personal, family from Person where ident=?;"
+query = "select personal, family from Person where ident='%s';"
 user_id = sys.argv[1]
 
 connection = sqlite3.connect("survey.db")
 cursor = connection.cursor()
 
-cursor.execute(query, [user_id])
+cursor.execute(query % user_id)
 results = cursor.fetchall()
 print results[0][0], results[0][1]
 
--- db/site_lat_long.py
+++ db/site_lat_long.py
@@ -1,11 +1,9 @@
 import sqlite3
-
-statement = "select site.lat, site.long from site;"
-
 connection = sqlite3.connect("survey.db")
 cursor = connection.cursor()
-cursor.execute(statement)
-for r in cursor.fetchall():
+cursor.execute("select site.lat, site.long from site;")
+results = cursor.fetchall()
+for r in results:
     print r
 cursor.close()
 connection.close()
