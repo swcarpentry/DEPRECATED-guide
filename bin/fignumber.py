93edac9d250aba923e4f75d20bdd6ec9339269d4
Greg Wilson <gvwilson@third-bit.com>
2013-05-06 10:09:12 -0400

    Cleaning up figure numbering script

--- bin/fignumber.py
+++ bin/fignumber.py
@@ -3,75 +3,62 @@
 import sys
 import re
 
-DEF_P = re.compile(r'<figure\s+id="(f:[^"]+)"[^>]*>')
-REF_P = re.compile(r'<a\s+href="#(f:[^"]+)">.*?</a>')
-FIG_P = re.compile(r'<figure\s+id="(f:[^"]+)"[^>]*>(\s+)<img\s+src="([^"]+)"\s+alt="([^"]+)"\s*/>(\s+)</figure>',
+#----------------------------------------
+
+DEF_P = re.compile(r'<figure id="(f:[^"]+)">(\s+)<img\s+src="(.+)"\s+alt="(.+)"\s*/>(\s+)<figcaption>.+</figcaption>(\s+)</figure>',
                    re.MULTILINE)
-FORMATTED_FIG = '<figure id="%(id)s">%(ws_1)s<img src="%(img)s" alt="%(cap)s" />%(ws_1)s<figcaption>Figure %(fig_num)d: %(cap)s</figcaption>%(ws_2)s</figure>'
+DEF_T = '<figure id="%(id)s">%(ws_1)s<img src="%(src)s" alt="%(caption)s" />%(ws_2)s<figcaption>Figure %(num)s: %(caption)s</figcaption>%(ws_3)s</figure>'
+REF_P = re.compile(r'<a href="#(f:[^"]+)">[^<]+</a>')
+REF_T = '<a href="#%(id)s">Figure %(num)d</a>'
 
-#-------------------------------------------------------------------------------
+#----------------------------------------
 
-def extract_defs(data):
-    """
-    Extract figure definitions, returning a dict mapping the anchor tag to
-    the figure number.
-    """
-    return dict((m, i+1) for (i, m) in enumerate(DEF_P.findall(data)))
+def main(reader, writer):
+    original = reader.read()
+    values = get_defs(original)
+    temp = subber(original, values, DEF_P, DEF_T)
+    final = subber(temp, values, REF_P, REF_T)
+    writer.write(final)
 
-#-------------------------------------------------------------------------------
+#----------------------------------------
 
-def update_refs(data, file_num, refs):
-    """
-    Update references, returning new document text.
-    """
-    def repl(m):
-        assert m.group(1) in refs, \
-               'Reference "%s" not defined in update_refs' % m.group(1)
-        return '<a href="#%s">Figure %d</a>' % \
-               (m.group(1), refs[m.group(1)])
-
-    return REF_P.sub(repl, data)
-
-#-------------------------------------------------------------------------------
-
-def update_figs(data, file_num, refs):
-    """
-    Update figures, returning new data.
-    """
-    def repl(m):
-        assert m.group(1) in refs, \
-               'Reference "%s" not defined in update_figs' % m.group(1)
-        vals = {
-            'id'       : m.group(1),
-            'ws_1'     : m.group(2),
-            'img'      : m.group(3),
-            'cap'      : m.group(4),
-            'ws_2'     : m.group(5),
-            'chap_num' : file_num,
-            'fig_num'  : refs[m.group(1)]
+def get_defs(original):
+    matches = DEF_P.findall(original)
+    result = {}
+    for (num, m) in enumerate(matches):
+        result[m[0]] = {
+            'num'     : num+1,
+            'id'      : m[0],
+            'ws_1'    : m[1],
+            'src'     : m[2],
+            'caption' : m[3],
+            'ws_2'    : m[4],
+            'ws_3'    : m[5]
         }
-        id_stem = vals['id'].split(':')[1]
-        file_stem = vals['img'].split('/')[-1].split('.')[0]
-        assert id_stem == file_stem, \
-               'ID stem %s != file stem %s' % (vals['id'], vals['img'])
-        return FORMATTED_FIG % vals
+    return result
 
-    return FIG_P.sub(repl, data)
+#----------------------------------------
 
-#-------------------------------------------------------------------------------
+def subber(original, values, pattern, template):
 
-def main(filenames):
-    for (i, f) in enumerate(filenames):
-        file_num = i + 1
-        with open(f, 'r') as reader:
-            data = reader.read()
-        refs = extract_defs(data)
-        data = update_refs(data, file_num, refs)
-        data = update_figs(data, file_num, refs)
-        with open(f, 'w') as writer:
-            writer.write(data)
+    def swap(m):
+        fig_id = m.group(1)
+        return template % values[fig_id]
 
-#-------------------------------------------------------------------------------
+    return pattern.sub(swap, original)
+
+#----------------------------------------
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    if len(sys.argv) == 1:
+        main(sys.stdin, sys.stdout)
+    elif len(sys.argv) == 2:
+        with open(sys.argv[1], 'r') as reader:
+            main(reader, sys.stdout)
+    elif len(sys.argv) == 3:
+        with open(sys.argv[1], 'r') as reader:
+            with open(sys.argv[2], 'w') as writer:
+                main(reader, writer)
+    else:
+        print >> sys.stderr, 'Usage: fignumber.py [infile [outfile]]'
+        sys.exit(1)
