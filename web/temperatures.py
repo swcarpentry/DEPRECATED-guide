1950aadfb6ad2951bb734c7ca1055646f3d8615f
Greg Wilson <gvwilson@third-bit.com>
2013-05-14 13:05:41 -0400

    Doing the download section of the web tutorial

--- web/temperatures.py
+++ web/temperatures.py
@@ -1,6 +1,8 @@
 import sys
 import requests
-import xml.etree.ElementTree as ET
+import json
+
+URL = 'http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/%s.JSON'
 
 def main(args):
     first_country = 'AUS'
@@ -9,41 +11,39 @@ def main(args):
         first_country = args[0]
     if len(args) > 1:
         second_country = args[1]
-    ratios(first_country, second_country)
 
-def ratios(first_country, second_country):
-    '''Show ratio of average temperatures for two countries over time.'''
-    first = get_temps(first_country)
-    second = get_temps(second_country)
-    assert len(first) == len(second), 'Length mis-match in results'
-    keys = first.keys()
+    result = ratios(first_country, second_country)
+    display(result)
+
+def display(values):
+    '''Show dictionary entries in sorted order.'''
+    keys = values.keys()
     keys.sort()
     for k in keys:
-        print k, first[k] / second[k]
+        print k, values[k]
+
+def ratios(first, second):
+    '''Calculate ratio of average temperatures for two countries over time.'''
+    first = get_temps(first)
+    second = get_temps(second)
+    assert len(first) == len(second), 'Length mis-match in results'
+    result = {}
+    for (i, first_entry) in enumerate(first):
+        year = first_entry['year']
+        second_entry = second[i]
+        assert second_entry['year'] == year, 'Year mis-match'
+        result[year] = first_entry['data'] / second_entry['data']
+    return result
 
 def get_temps(country_code):
     '''Get annual temperatures for a country.'''
-    doc = get_xml(country_code)
-    result = {}
-    for element in doc.findall('domain.web.V1WebCru'):
-        year = find_one(element, 'year').text
-        temp = find_one(element, 'data').text
-        result[int(year)] = kelvin(float(temp))
+    response = requests.get(URL % country_code)
+    assert response.status_code == 200, \
+           'Failed to get data for %s' % country_code
+    result = json.loads(response.text)
+    for entry in result:
+        entry['data'] = kelvin(entry['data'])
     return result
-
-def get_xml(country_code):
-    '''Get XML temperature data for a country.'''
-    url = 'http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/%s.XML'
-    u = url % country_code
-    response = requests.get(u)
-    doc = ET.fromstring(response.text)
-    return doc
-
-def find_one(node, pattern):
-    '''Get exactly one child that matches an XPath pattern.'''
-    all_results = node.findall(pattern)
-    assert len(all_results) == 1, 'Got %d children instead of 1' % len(all_results)
-    return all_results[0]
 
 def kelvin(celsius):
     '''Convert degrees C to degrees K.'''
